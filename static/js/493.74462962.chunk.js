3de56c16981eb37babd59b6ac16380cbbd731cec
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-05-22 10:56:45 +0000

    Deploying to gh-pages from @ YuliiaSliepchenko/goit-react-hw-08-phonebook@0ea4251a67dafc08f7e0acefde3ae3ac6a4bd226 🚀

---
+++ static/js/493.74462962.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[493],{7493:function(e,t,o){o.r(t),o.d(t,{default:function(){return n}});var r=o(3329);function n(){return(0,r.jsx)("h1",{children:"Wellcome to your phonebook!"})}}}]);
+//# sourceMappingURL=493.74462962.chunk.js.map
--- static/js/493.849790e2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[493],{7493:function(e,t,o){o.r(t),o.d(t,{default:function(){return n}});var r=o(3329);function n(){return(0,r.jsx)("h1",{children:"Wellcome to your personal phonebook"})}}}]);
-//# sourceMappingURL=493.849790e2.chunk.js.map
